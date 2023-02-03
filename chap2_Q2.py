178b668699c7fccd94aea28969bf1c0a4ed4e676
ksj1368 <83360918+ksj1368@users.noreply.github.com>
2023-02-03 14:53:48 +0900

    Update chap2_Q2.py

--- chap2_Q2.py
+++ chap2_Q2.py
@@ -1,5 +1,5 @@
 # Q2 Answer template
-
+### 방법1.
 def solution2(numbers):
     answer = 0
     arr = [i for i in range(1, 10)]
@@ -7,6 +7,16 @@ def solution2(numbers):
     for n in arr:
         if n not in numbers:
             answer += n
+    return answer
+
+# numbers = [1,2,3,4,6,7,8,0]
+numbers = [5,8,4,0,6,7,9]
+solution2(numbers)
+
+### 방법2.
+def solution2(numbers):
+    arr = [i for i in range(1, 10)]
+    answer = sum(arr)-sum(numbers)
     return answer
 
 # numbers = [1,2,3,4,6,7,8,0]
