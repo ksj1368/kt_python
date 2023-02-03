b4a8311b8b558b2182d19432da24f204f61e2e34
ksj1368 <83360918+ksj1368@users.noreply.github.com>
2023-02-03 16:17:24 +0900

    Update chap2_Q6.py

--- chap2_Q6.py
+++ chap2_Q6.py
@@ -1,9 +1,11 @@
 # Q6 Answer template
 def solution6(arr):
-    minimum = 99999
-    for i in arr:
+    minimum = arr[0]
+    
+    for i in arr[1:]:
         if minimum > i:
             minimum = i
+    
     arr.remove(minimum)
     
     if arr:
@@ -11,6 +13,6 @@ def solution6(arr):
     else:
         return [-1]
     
-# arr = [4, 3, 2, 1]
-arr = [10]
+arr = [4, 3, 2, 1]
+#arr = [10]
 solution6(arr)
